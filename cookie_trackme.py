e1ef9fca419aba288d75d285aaa32d660af2edab
SpyrosSpiliopoulos <spiliopoulos.spiridon@gmail.com>
2020-01-11 21:39:18 +0200

    trackme is now more coherent in terms of what it does

--- cookie_trackme.py
+++ cookie_trackme.py
@@ -9,6 +9,18 @@ from chrome_connector import PyChromeConnector
 from cookie_visualizer import visualize_tohtml_audit
 
 
+def ask_url_and_get_alias():
+    url=input("Enter url : ")
+    alias_question=None
+    while alias_question not in ("yes", "no"):
+        alias_question=input("do you want to alias the domain name? (yes or no): ")
+    alias=None
+    if alias_question=="yes":
+        alias=input("provide an alias to the domain name: ")
+    return url,alias
+
+def get_domain(url):
+    return url.lower().split("www")[-1].split("/")[0]
 
 if __name__=="__main__":
 
@@ -18,7 +30,8 @@ if __name__=="__main__":
     chrome_session = PyChromeConnector(port=9222)
 
     graphmap_inter={"source":[],"target":[],"weight":[],"profile_type":[],"domain":[]}
-    url=input("Enter url : ")
+
+    url,alias=ask_url_and_get_alias()
     chrome_session.go_page(url)
 
     try:
@@ -29,22 +42,29 @@ if __name__=="__main__":
 
                     answer = input("Do you want to fetch cookies? (yes or no): ")
                     if answer == "yes":
-                        profile_type=input("What is the cookie profile? (provide description i.e. necessary, optional...):" )
+                        profile_type=input("What is the cookie profile? (provide description i.e. necessary, optional...): " )
                         cookies=chrome_session.get_cookies()
                         for cookie in cookies:
-                            domain=url.lower().split("www")[-1]
+                            if not alias:
+                                domain=get_domain(url)
+                            else:
+                                domain=alias
                             graphmap_inter["source"].append(domain)
-                            graphmap_inter["target"].append(cookie["domain"])
-                            graphmap_inter["weight"].append(1)
-                            graphmap_inter["source"].append(cookie["domain"])
-                            graphmap_inter["target"].append(cookie['name'])
-                            graphmap_inter["weight"].append(0.3)
+                            graphmap_inter["target"].append(cookie["name"])
+                            graphmap_inter["weight"].append(.01)
+                            graphmap_inter["profile_type"].append(profile_type)
+                            graphmap_inter["domain"].append(domain)
+                            graphmap_inter["source"].append(cookie['name'])
+                            graphmap_inter["target"].append(get_domain(cookie["domain"]))
+                            graphmap_inter["weight"].append(.01)
                             graphmap_inter["profile_type"].append(profile_type)
                             graphmap_inter["domain"].append(domain)
                         chrome_session.clear_cookies()
                         chrome_session.go_page(url)
                     elif answer == "no":
-                         continue
+                        visualize_tohtml_audit(graphmap_inter)
+                        url,alias=ask_url_and_get_alias()
+                        chrome_session.go_page(url)
                     else:
                     	print("Please enter yes or no.")
     except KeyboardInterrupt:
@@ -52,5 +72,4 @@ if __name__=="__main__":
     except Exception as e:
         print(e)
 
-    visualize_tohtml_audit(graphmap_inter)
     p.kill()
--- cookie_visualizer.py
+++ cookie_visualizer.py
@@ -3,6 +3,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import mpld3
 import re
+import matplotlib, matplotlib.pyplot as plt
+
 
 
 def visualize_tohtml_globalmap(graphmap_inter):
@@ -45,10 +47,18 @@ def visualize_tohtml_audit(graphmap_inter):
     profiletypes = graphmap_inter['profile_type']
     domains = graphmap_inter['domain']
 
+    unique_profiles=list(set(profiletypes))
+    #get edge colors unique for each profiletype
+    n = len(unique_profiles)
+    from_list = matplotlib.colors.LinearSegmentedColormap.from_list
+    cm = from_list('Set15', plt.cm.Set2(range(0,n)), n)
+
+    map_profile_toint={key:val for key, val in zip(unique_profiles,range(n))}
+
     edge_data = zip(sources, targets, weights,profiletypes,domains)
 
 
-    net = Network(height="750px", width="100%", bgcolor="#222222", font_color="white",directed=True)
+    net = Network(height="750px", width="100%", bgcolor="#011936", font_color="white",directed=True)
 
     for i,e in enumerate(edge_data):
         src = e[0]
@@ -57,16 +67,30 @@ def visualize_tohtml_audit(graphmap_inter):
         prf = e[3]
         dom = e[4]
         dom_annotated=dom+"("+prf+")"
+        if src==dom:
+            src=dom_annotated
         if dst==dom:
             dst=dom_annotated
-        if src==dst:
+        if src==dst and src!=dom:
             c="green"
         elif src!=dst:
-            c="red"
-        net.add_node(dom_annotated, dom_annotated, title="<font size='4'>%s</font>"%(src),color="blue")
-        #make distinct nodes per audit profile
-        net.add_node(dst, dst, title=dst)
-        net.add_edge(dom_annotated, dst, value=0.1,color=c)
+            c=matplotlib.colors.to_hex(cm(map_profile_toint[prf]))
+
+        if src==dom_annotated:
+            net.add_node(dom_annotated, dom_annotated, title="<font size='4'>%s</font>"%(src),color="#F9DC5C",size=3,physics=False)
+            net.add_node(dst, dst, title=dst)
+            net.add_edge(dom_annotated, dst, value=w,color=c)
+        elif dst==dom_annotated:
+            net.add_node(src, src, title="<font size='4'>%s</font>"%(src),color="#F9DC5C",size=3,physics=False)
+            net.add_node(dom_annotated, dom_annotated, title=dst)
+            net.add_edge(src, dom_annotated, value=w,color=c)
+        else:
+            net.add_node(src, src, title="<font size='4'>%s</font>"%(src),physics=False)
+            net.add_node(dst, dst, title=dst,color="#D81159",physics=False)
+            net.add_edge(src, dst, value=w,color="#7A6C5D")
+
+    net.force_atlas_2based(gravity=-161,spring_length=100,damping=0.27,overlap=1)
+
 
     neighbor_map = net.get_adj_list()
 
